d5e05c27728e7e67138afdd3f7bfc3ea229acde2
subinsong01 <haely97@naver.com>
2024-09-22 23:31:59 +0900

    Design: 로그인 페이지 UI 완료

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import ReactDOM from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 import LandingPage from "./components/LandingPage";
+import LoginPage from "./components/LoginPage";
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <GoogleOAuthProvider clientId={clientId}>
-    <App />
+    <LoginPage />
   </GoogleOAuthProvider>
 );
